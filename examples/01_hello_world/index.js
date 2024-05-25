442775f63b23fb867535c498be48d23c4bd3bb9d
jt000 <jt000@users.noreply.github.com>
2024-05-25 14:37:31 -0400

    removing examples now in their own repo

--- examples/01_hello_world/index.js
+++
@@ -1,4 +0,0 @@
-// This is a simple "hello world" script to demonstrate 
-// a very basic app running in pico-sdk-js
-
-print('hello world!');
--- examples/02_hardware_timer/index.js
+++
@@ -1,4 +0,0 @@
-import { time_us_32 } from 'pico/hardware';
-
-const usTime32 = time_us_32();
-print(`Time in microseconds: ${usTime32}`);
