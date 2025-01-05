f78a2000ec100b73fdee20aa4fd597f2540522f9
Jason Tremper <jt000@users.noreply.github.com>
2025-01-05 11:27:25 -0500

    Fixing issue in write command including trailing 0s in content (#17)
    Closes #16

--- e2e/tests/cli.flash.test.ts
+++ e2e/tests/cli.flash.test.ts
@@ -45,6 +45,34 @@ describe('PSJ Flash Scenarios', () => {
             .command('.read test1.txt')
             .assertSnapshot();
         });
+
+        it('will write a JS file', async () => {
+            const fileText = `(()=>{"use strict";print("hello world!")})();
+//# sourceMappingURL=index.js.map`;
+
+            const tempFile = path.join(os.tmpdir(), 'index.js');
+            await fs.writeFile(tempFile, fileText);
+
+            await psjRunner()
+            .start(['--auto-connect', '--skip-header'])
+            .command(`.write index.js -p "${tempFile}"`)
+            .command('.run index.js')
+            .assertSnapshot();
+        });
+        
+        it('will write a JS file from another directory without path arguement', async () => {
+            const fileText = `(()=>{"use strict";print("hello world!")})();
+//# sourceMappingURL=index.js.map`;
+
+            const tempFile = path.join(os.tmpdir(), 'index.js');
+            await fs.writeFile(tempFile, fileText);
+
+            await psjRunner()
+            .start(['--auto-connect', '--skip-header'])
+            .command(`.write ${tempFile}`)
+            .command('.run index.js')
+            .assertSnapshot();
+        });
         
         it('will error if file is larger than available space', async () => {
             // 1mb file
@@ -53,14 +81,10 @@ describe('PSJ Flash Scenarios', () => {
             const tempFile = path.join(os.tmpdir(), 'test2.txt');
             await fs.writeFile(tempFile, fileText);
 
-            const procStart = process.hrtime();
-
             await psjRunner()
             .start(['--auto-connect', '--skip-header'])
             .command(`.write test2.txt --local-path "${tempFile}"`)
             .assertSnapshot();
-
-            console.log(`Perf Time: ${procStart[0] * 1000 + procStart[1] / 1000000}ms`)
         }, 150000);
         
         xit.failing('cannot write a hidden file', async () => {
--- src/commands/writeCommand.ts
+++ src/commands/writeCommand.ts
@@ -32,11 +32,16 @@ class FileReader implements IReader {
             this._fileHandle = await fs.open(this._srcName, 'r');
         }
 
-        const buffer = new Int8Array(size);
+        const buffer = Buffer.alloc(size);
         const result = await this._fileHandle.read(buffer, 0, size);
+
+        if (result.bytesRead === 0) {
+            return { bytesRead: 0, content: '' };
+        }
+
         return {
             bytesRead: result.bytesRead,
-            content: this._decoder.decode(result.buffer)
+            content: result.buffer.toString('utf-8', 0, result.bytesRead)
         };
     }
 }
@@ -108,7 +113,7 @@ export async function writeCommand(replServer: PsjReplServer, text: string): Pro
         .exitProcess(false);
 
     yargs.example('.write file.js --local-path ./myFile.js', 'write the local "myFile.js" to the Pico with the file name "file.js".');
-    yargs.example('.write file.js', 'write the local "file.js" to the Pico with the file name "file.js".');
+    yargs.example('.write /tmp/file.js', 'write the local "/tmp/file.js" to the Pico with the file name "file.js".');
 
     const args = await yargs.parseAsync();
 
@@ -121,7 +126,8 @@ export async function writeCommand(replServer: PsjReplServer, text: string): Pro
         throw new Error('Not connected, run .connect to connect to a device running Pico-Sdk-JS.');
     }
 
-    const destName = args.remotePath;
+    // if no localPath nor content is explicitly given, then remotePath is both the local path and remote file name
+    const destName = args.localPath || args.content ? args.remotePath : path.basename(args.remotePath);
     const reader = args.content ? new StringReader(args.content) : args.localPath ? new FileReader(args.localPath) : new FileReader(args.remotePath);
     const pageSize = 1024;
     let bytesWritten = 0;
