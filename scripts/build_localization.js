af363d75b2e5e22f12baaefd93d9e287b9b4deda
jt000 <jasontre@microsoft.com>
2025-01-22 11:07:49 -0500

    Removing build localization script

--- scripts/build_localization.js
+++
@@ -1,4 +0,0 @@
-
-// const translateRegEx = /(?<![a-zA-Z0-9])t\('(?<str>[^']+)'(?<args>, [^)]*)?\)/g;
-
-// for all *.ts files under ../src, search for t('..', args) and make sure that it is available in the locate files
